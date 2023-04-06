0ed39dcf1a8f89d190a65601fb72cb0328e49f26
Thinkedou <r.enguehard@gmail.com>
2023-04-06 14:44:21 +0200

    Adding boostrap buttons

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -21,5 +21,37 @@ onBeforeMount(async ()=>{
 </script>
 
 <template>
-    <h3>ADMIN HOME</h3>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>
+        <button type="button" class="btn btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+        <button type="button" class="btn btn-danger ml-2"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+        </td>
+    </tr>
+  </tbody>
+</table>
 </template>
